e9710cf9e6d0142ff0ff35eeef2ff8f61384dd1e
= <=>
2026-01-12 18:14:09 +0900

    260112 - json query 실습 (json data 확인)

--- src/test.py
+++ src/test.py
@@ -1,23 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 class WebResponse:
     def __init__(self):
-        self.default_site = None
+        pass
 
-    def response_html(self, site = None):
+    def response_html(self, site = None, header = None):
         target_url = site if site else self.default_site
+
         try:
             response = requests.get(target_url)
             response.raise_for_status()
-
+            # print(response.text)
             soup = BeautifulSoup(response.text, 'html.parser')
 
             title = soup.title.string if soup.title else 'No title'
+            
+            json_data = json.loads(response.content)
+            print(json_data)
+            # print(soup) 
 
-            print(' -- [success] --')
+            # print(' -- [success] --')
             # print(f"URL = {target_url}")
-            print(f"title = {title}")
+            # print(f"title = {title}")
 
             return soup
         
@@ -27,12 +33,21 @@ class WebResponse:
     
         
 def main():
-    url = 'https://www.google.com'
+    url = 'https://comic.naver.com/api/webtoon/titlelist/weekday?order=user'
+    referer = 'https://comic.naver.com/webtoon'
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36'
+    headers = {'referer' : referer, 'user-agent' : user_agent}
     web = WebResponse()
     try:
-        web.response_html(url)
+        web.response_html(url, headers)
     except KeyboardInterrupt:
         pass
 
 if __name__ == '__main__':
     main()
+
+'''
+request url = https://comic.naver.com/api/webtoon/titlelist/weekday?order=user
+referer = https://comic.naver.com/webtoon
+user_agnet = Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36
+'''

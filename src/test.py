86e1be1f86995bde9a79eb0205aa5a0d2ec5cceb
= <=>
2026-01-12 19:27:16 +0900

    260112 - json query 실습 (요일별 웹툰 제목 & 별점 커스텀 딕셔너리 생성)

--- src/test.py
+++ src/test.py
@@ -4,22 +4,28 @@ import json
 
 class WebResponse:
     def __init__(self):
-        pass
+        self.default_site = ""
+        self.headers = {}
+        self.datas = {}
 
     def response_html(self, site = None, header = None):
         target_url = site if site else self.default_site
-
+        # print(header)
         try:
             response = requests.get(target_url)
             response.raise_for_status()
             json_data = json.loads(response.content)
             week = [*json_data["titleListMap"].keys()]
             for w in week:
+                data_title, data_score = [], []
                 contents =[*json_data['titleListMap'][w]]
-                print(f"\n ===== 요일 : {w} ====== \n")
+                # print(f"\n ===== 요일 : {w} ====== \n")
                 for i in contents:
-                    print(f"제목 : {i['titleName']} , 별점 = {round(i['starScore'],2)} 점")
-            return json_data
+                    # print(f"요일 : {w}, 제목 : <<{i['titleName']}>> , 별점 = {round(i['starScore'],2)} 점")
+                    data_title.append(i['titleName'])
+                    data_score.append(i['starScore'])
+                    self.datas[w] = {'title' : data_title, 'score' : data_score}
+            return self.datas
         
         except requests.exceptions.RequestException as e:
             print(f"Error = {e}")
